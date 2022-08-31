5a0dda984eb3ae12e3deb073dcb56b725952e02c
Emilene Goulart <emigoulart@gmail.com>
2022-08-31 09:14:52 -0300

    Ajustes catalago de produtos

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -10,6 +10,12 @@ const ProductCard = () => {
             </div>
             <div className="card-bottom-container">
                 <h6>Audi Supra TT</h6>
+                <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+                </div>
+                <div>
+                    <button className="btn btn-info">Comprar</button>
+                </div>
             </div>
         </div>
     );
